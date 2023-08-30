8cfaad7e00380b2fff3715aa5fcf68d9830d0f96
Alvin Tsang <alvintsang@Alvins-MacBook-Pro-2.local>
2023-08-30 12:08:05 -0400

    added welcome text animation to fit quor vibe, bolded grab quote element, will work on generate quote button next

--- src/components/QuoteGen.js
+++ src/components/QuoteGen.js
@@ -8,22 +8,29 @@ import React from "react";
 
 function QuoteGen() {
     return (
+    <div className = "containers"> {/* react uses className, while regular html uses class*/}
+        <div className = "welcome_container">
+            <h1 className = "sparkle_welcome"> Welcome!</h1>
+            <p className = "grab_quote"> Grab a quote below!</p>
+        </div>
+
         <div className = "quote_container">
-            <img src = "/images/image1.png" alt = "Centered Image"/>
-            <h1>test</h1>
-            <p>test</p>
+            {/* <img src = "/images/image1.png" alt = "Centered Image"/> */}
+            {/* <h1>test</h1>
+            <p>test</p> */}
             <div className = "quote_btns">
-                <Button className = "btns" buttonStyle = "btn--outline"
+                {/* <Button className = "btns" buttonStyle = "btn--outline"
                 buttonSize = "btn--large">GET STARTED
-                </Button>
-                <Button className = "btns" buttonStyle = "btn--outline"
+                </Button> */}
+                {/* <Button className = "btns" buttonStyle = "btn--outline"
                 buttonSize = "btn--large">GENERATE QUOTE!
-                <i className = "farfa-play-circle"/>
-                </Button>
+                <i className = "far fa-play-circle"/>
+                </Button> */}
             </div>
         
         
         </div>
+    </div>
 
     );
 }
